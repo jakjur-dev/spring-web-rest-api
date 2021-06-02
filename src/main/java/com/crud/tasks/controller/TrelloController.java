35de8722e768b89a7c65558dcea58b1da3b79ba6
Jakub <jurak2012@gmail.com>
2021-06-02 17:58:50 +0200

    22.2 Organised the code

---
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -0,0 +1,31 @@
+package com.crud.tasks.controller;
+
+import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.trello.client.TrelloClient;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/v1/trello")
+@RequiredArgsConstructor
+public class TrelloController {
+
+    private final TrelloClient trelloClient;
+
+    @GetMapping("getTrelloBoards")
+    public void getTrelloBoards() {
+
+        List<TrelloBoardDto> trelloBoards = trelloClient.getTrelloBoards();
+
+        trelloBoards.stream()
+                .filter(b -> b.getId() != null)
+                .filter(f -> f.getName().contains("Kodilla"))
+                .forEach(t -> {
+                    System.out.println(t.getId() + " " + t.getName());
+                });
+    }
+}
