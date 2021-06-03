23fd7f908b2ccf961a1b649e81a26aae4fc2e25f
Jakub <jurak2012@gmail.com>
2021-06-03 17:58:18 +0200

    Added TrelloDto, AttachmentsByTypeDto and BadgesDto

--- src/main/java/com/crud/tasks/controller/TrelloController.java
+++ src/main/java/com/crud/tasks/controller/TrelloController.java
@@ -1,11 +1,11 @@
 package com.crud.tasks.controller;
 
+import com.crud.tasks.domain.CreatedTrelloCard;
 import com.crud.tasks.domain.TrelloBoardDto;
+import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,8 +24,20 @@ public class TrelloController {
         trelloBoards.stream()
                 .filter(b -> b.getId() != null)
                 .filter(f -> f.getName().contains("Kodilla"))
-                .forEach(t -> {
-                    System.out.println(t.getId() + " " + t.getName());
+                .forEach(trelloBoardDto -> {
+                    System.out.println(trelloBoardDto.getId() + " - " + trelloBoardDto.getName());
+                    System.out.println("This board contains lists: ");
+
+                    trelloBoardDto.getLists().forEach(trelloList -> {
+                        System.out.println(trelloList.getName() + " - " + trelloList.getId() + " - " + trelloList.isClosed());
+                    });
                 });
+
     }
+
+    @PostMapping("createTrelloCard")
+    public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
+        return trelloClient.createNewCard(trelloCardDto);
+    }
+
 }
