ee05566ca76d8a49b0556d8e4a54ca74b50e3f4b
Jakub <jurak2012@gmail.com>
2021-06-10 19:23:57 +0200

    Added default constructor for Created Trello Card

--- src/main/java/com/crud/tasks/domain/TrelloCardDto.java
+++ src/main/java/com/crud/tasks/domain/TrelloCardDto.java
@@ -2,8 +2,10 @@ package com.crud.tasks.domain;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
+@NoArgsConstructor
 @AllArgsConstructor
 public class TrelloCardDto {
 
